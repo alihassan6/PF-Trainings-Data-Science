41c777283336ab9e4a7d24dbfe9fc247387759d2
alihassan6 <102224212+alihassan6@users.noreply.github.com>
2023-03-06 18:59:26 +0500

    Add files via upload

---
+++ cv2_red pixels replace.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+import cv2
+import numpy as np
+from PIL import Image
+
+im_array = cv2.imread('imagee.png')
+display(Image.fromarray(cv2.cvtColor(im_array, cv2.COLOR_BGR2RGB)))
+
+hsv = cv2.cvtColor(im_array, cv2.COLOR_BGR2HSV)
+
+red_lower_range = np.array([0, 100, 100])
+red_upper_range = np.array([9, 255, 255])
+
+mask_0 = cv2.inRange(hsv, red_lower_range, red_upper_range)
+
+red_lower_range = np.array([160, 100, 100])
+red_upper_range = np.array([179, 255, 255])
+
+mask_1 = cv2.inRange(hsv, red_lower_range, red_upper_range)
+mask_2 = mask_0 + mask_1
+
+im_array[np.where(mask_2 == 255)] = [0, 0, 0]
+
+display(Image.fromarray(cv2.cvtColor(im_array, cv2.COLOR_BGR2RGB)))
+
+
+# In[ ]:
+
+
+
+
