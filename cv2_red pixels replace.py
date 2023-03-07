cd306f0caa3ce802d38689d8e9a7e2464c49c50b
alihassan6 <102224212+alihassan6@users.noreply.github.com>
2023-03-07 09:55:41 +0500

    Update cv2_red pixels replace.py

--- cv2_red pixels replace.py
+++ cv2_red pixels replace.py
@@ -18,6 +18,10 @@ red_upper_range = np.array([179, 255, 255])
 mask_1 = cv2.inRange(hsv, red_lower_range, red_upper_range)
 mask_2 = mask_0 + mask_1
 
-im_array[np.where(mask_2 == 255)] = [0, 0, 0]
+for i in range(im_array.shape[0]):
+    for j in range(im_array.shape[1]):
+        if mask_2[i, j] == 255:
+            im_array[i, j] = [0, 0, 0]
+
 
 display(Image.fromarray(cv2.cvtColor(im_array, cv2.COLOR_BGR2RGB)))
